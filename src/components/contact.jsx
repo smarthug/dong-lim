9aab313b530de14c1ae7454b31e178f1dc4074d5
smarthug <kirklayer@gmail.com>
2023-03-08 21:43:18 +0900

    clear warning

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import emailjs from "emailjs-com";
 import React from "react";
 
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
+// const initialState = {
+//   name: "",
+//   email: "",
+//   message: "",
+// };
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  // const [{ name, email, message }, setState] = useState(initialState);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({ ...prevState, [name]: value }));
-  };
-  const clearState = () => setState({ ...initialState });
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setState((prevState) => ({ ...prevState, [name]: value }));
+  // };
+  // const clearState = () => setState({ ...initialState });
 
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    // console.log(name, email, message);
-    // emailjs
-    //   .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //       clearState();
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
-  };
+  // const handleSubmit = (e) => {
+  //   // e.preventDefault();
+  //   // console.log(name, email, message);
+  //   // emailjs
+  //   //   .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+  //   //   .then(
+  //   //     (result) => {
+  //   //       console.log(result.text);
+  //   //       clearState();
+  //   //     },
+  //   //     (error) => {
+  //   //       console.log(error.text);
+  //   //     }
+  //   //   );
+  // };
   return (
     <div style={
       {
