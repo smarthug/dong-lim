0a5c7745dac3824c2e6b2b99db4ed7ecf2df45ae
smarthug <kirklayer@gmail.com>
2023-03-08 21:23:07 +0900

    almost done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,13 +8,22 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                
+
                 <h1>
-                  
+
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <p
+                  style={{
+
+                    // height: "10px"
+                  }}
+
+                >
+                  {/* {props.data ? props.data.paragraph : "Loading"} */}
+                
+                </p>
                 <a
                   href="#contact"
                   className="btn btn-custom btn-lg page-scroll"
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -17,8 +17,10 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
+
+          <img src="img/logo.jpg" />
           <a className="navbar-brand page-scroll" href="#page-top">
-            동림엔텍(주)
+            {/* 동림엔텍(주) */}
           </a>{" "}
         </div>
 
@@ -27,13 +29,13 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-          
+
             <li>
               <a href="#portfolio" className="page-scroll">
                 제품
               </a>
             </li>
-           
+
             <li>
               <a href="#contact" className="page-scroll">
                 연락처
